fd581d84720d07d5960409dbe8f7b17cd5e17aa9
Denis Laliberte <denis.laliberte@gmail.com>
2017-08-26 11:46:25 -0400

    first try with cond

---
+++ elixir/protein-translation/protein_translation.exs
@@ -0,0 +1,45 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: { atom,  list(String.t()) }
+  def of_rna(rna) do
+    rna
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: { atom, String.t() }
+  def of_codon(codon) do
+    cond do
+      codon == "AUG" ->
+        { :ok, "Methionine" }
+      codon == "UUU" || codon == "UUC" ->
+        { :ok, "Phenylalanine" }
+      codon == "UUA" || codon == "UUG" ->
+        { :ok, "Leucine" }
+      codon == "UCU" || codon == "UCC" || codon == "UCA" || codon == "UCG" ->
+        { :ok, "Serine" }
+    end
+  end
+end
+
