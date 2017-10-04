9a7d08a5d463cdb1f89dafd3383e7069c31820e5
Denis Laliberte <denis.laliberte@gmail.com>
2017-10-03 22:39:50 -0400

    roman immutable functionnal

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,49 +1,47 @@
-require 'pry'
-
 class Integer
   def to_roman
-    converter = Converter.new(self)
-    translation.each do |number, roman|
-      converter = converter.convert(number, roman)
-    end
-    converter.roman
+    translations
+      .inject(Translator.new(number: self)) do |candidate, translation|
+        candidate.translate(translation)
+      end.roman
   end
 
   private
 
-  class Converter
-    attr_accessor :roman
-    def initialize(number, roman: '')
+  def translations
+    translation = Struct.new(:number, :roman)
+    [
+      translation.new(1000, 'M'),
+      translation.new(900, 'CM'),
+      translation.new(500, 'D'),
+      translation.new(400, 'CD'),
+      translation.new(100, 'C'),
+      translation.new(90, 'XC'),
+      translation.new(50, 'L'),
+      translation.new(40, 'XL'),
+      translation.new(10, 'X'),
+      translation.new(9, 'IX'),
+      translation.new(5, 'V'),
+      translation.new(4, 'IV'),
+      translation.new(1, 'I')
+    ]
+  end
+
+  class Translator
+    attr_accessor :number, :roman
+    def initialize(number: 0, roman: '')
       @number = number
       @roman = roman
     end
 
-    def convert(number, roman)
-      if @number >= number
-        return Converter
-               .new(@number - number, roman: @roman + roman)
-               .convert(number, roman)
-      end
-      self
+    def translate(translation)
+      return self if number < translation.number
+      Translator
+        .new(
+          number: number - translation.number,
+          roman: roman + translation.roman
+        ).translate(translation)
     end
-  end
-
-  def translation
-    [
-      [1000, 'M'],
-      [900, 'CM'],
-      [500, 'D'],
-      [400, 'CD'],
-      [100, 'C'],
-      [90, 'XC'],
-      [50, 'L'],
-      [40, 'XL'],
-      [10, 'X'],
-      [9, 'IX'],
-      [5, 'V'],
-      [4, 'IV'],
-      [1, 'I']
-    ]
   end
 end
 
