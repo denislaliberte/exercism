e6df05982cf4758825f310df989982c08e5864dd
Denis Laliberte <denis.laliberte@gmail.com>
2017-10-04 21:57:20 -0400

    no comment

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,47 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'csv'
+
+# add to_roman to Integer class
 class Integer
   def to_roman
-    translations
-      .inject(Translator.new(number: self)) do |candidate, translation|
-        candidate.translate(translation)
-      end.roman
+    remaining = self
+    temp = ''
+    translation.each do |number, roman|
+      while remaining >= number
+        temp << roman
+        remaining -= number
+      end
+    end
+    temp
   end
 
   private
 
-  def translations
-    translation = Struct.new(:number, :roman)
-    [
-      translation.new(1000, 'M'),
-      translation.new(900, 'CM'),
-      translation.new(500, 'D'),
-      translation.new(400, 'CD'),
-      translation.new(100, 'C'),
-      translation.new(90, 'XC'),
-      translation.new(50, 'L'),
-      translation.new(40, 'XL'),
-      translation.new(10, 'X'),
-      translation.new(9, 'IX'),
-      translation.new(5, 'V'),
-      translation.new(4, 'IV'),
-      translation.new(1, 'I')
-    ]
+  def translation
+    CSV
+      .read('roman_numerals.csv')
+      .map { |a, b| [a.to_i, b.strip] }
+      .sort_by(&:first)
+      .reverse
   end
+end
 
-  class Translator
-    attr_accessor :number, :roman
-    def initialize(number: 0, roman: '')
-      @number = number
-      @roman = roman
-    end
-
-    def translate(translation)
-      return self if number < translation.number
-      Translator
-        .new(
-          number: number - translation.number,
-          roman: roman + translation.roman
-        ).translate(translation)
-    end
+describe Integer do
+  describe '#to_roman' do
+    it { 1.to_roman.must_equal 'I' }
+    it { 2.to_roman.must_equal 'II' }
+    it { 3.to_roman.must_equal 'III' }
+    it { 5.to_roman.must_equal 'V' }
+    it { 10.to_roman.must_equal 'X' }
+    it { 20.to_roman.must_equal 'XX' }
   end
 end
 
