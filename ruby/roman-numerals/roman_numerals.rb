bf011913a925db732af9379916ade24b067390a5
Denis Laliberte <denis.laliberte@gmail.com>
2017-10-04 22:21:45 -0400

    no comment

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,12 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/spec'
-require 'csv'
+require 'CSV'
 
-# add to_roman to Integer class
+# add to_roman to Integerclass
 class Integer
   def to_roman
-    remaining = self
     temp = ''
+    remaining = self
     translation.each do |number, roman|
       while remaining >= number
         temp << roman
@@ -21,21 +21,19 @@ class Integer
   def translation
     CSV
       .read('roman_numerals.csv')
-      .map { |a, b| [a.to_i, b.strip] }
-      .sort_by(&:first)
-      .reverse
+      .map { |number, roman| [number.to_i, roman.strip] }
   end
 end
 
 describe Integer do
-  describe '#to_roman' do
-    it { 1.to_roman.must_equal 'I' }
-    it { 2.to_roman.must_equal 'II' }
-    it { 3.to_roman.must_equal 'III' }
-    it { 5.to_roman.must_equal 'V' }
-    it { 10.to_roman.must_equal 'X' }
-    it { 20.to_roman.must_equal 'XX' }
-  end
+  it { 1.to_roman.must_equal 'I' }
+  it { 2.to_roman.must_equal 'II' }
+  it { 3.to_roman.must_equal 'III' }
+  it { 5.to_roman.must_equal 'V' }
+  it { 6.to_roman.must_equal 'VI' }
+  it { 10.to_roman.must_equal 'X' }
+  it { 11.to_roman.must_equal 'XI' }
+  it { 20.to_roman.must_equal 'XX' }
 end
 
 module BookKeeping
